0d40bc978180fcf6bd3dacc42ce2e52975509adf
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-02-28 07:13:01 +0500

    filest settings

--- conf/urls.py
+++ conf/urls.py
@@ -1,8 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+
+from conf import settings
 
 urlpatterns = [
-    path('/', include('blogs.urls',namespace='blogs')),  # Add blog app URLs here
     path('admin/', admin.site.urls),
 ]
+
